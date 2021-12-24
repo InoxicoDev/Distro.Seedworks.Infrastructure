dee8d52b937e672524595f2599d0c501569f5ab9
Marius Vorster <marius.vrstr@gmail.com>
2021-12-24 08:03:56 +0200

    Added new db test base class

---
+++ Distro.Seedworks.Infrastructure/DataAccess/DbTestsBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Transactions;
+
+namespace Distro.Seedworks.Infrastructure.DataAccess
+{
+    public abstract class DbTestsBase<T> : IDisposable
+        where T : DbContext
+    {
+        protected T _context;
+        private TransactionScope _transaction;
+
+        public DbTestsBase(string connectionStringName = "DefaultConnection")
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<T>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            _context = (T)Activator.CreateInstance(typeof(T), optionsBuilder.Options);
+
+            _transaction = new TransactionScope(TransactionScopeOption.RequiresNew, TimeSpan.MaxValue);
+        }
+
+        public void Dispose()
+        {
+            _transaction.Dispose();
+        }
+    }
+}
