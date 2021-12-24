baf32f5f54cd3593b2a44a427ac0f63f17ecf26d
Marius Vorster <marius.vrstr@gmail.com>
2021-12-24 07:41:12 +0200

    Added database unit test base class

--- Distro.Seedworks.Infrastructure/Domain/EntityBase.cs
+++ Distro.Seedworks.Infrastructure/Domain/EntityBase.cs
@@ -3,6 +3,6 @@ namespace Distro.Seedworks.Infrastructure.Domain
 {
     public class EntityBase<T> : IDomainEntity
     {
-        public T? Behaviors { get; set; }
+        //public T? Behaviors { get; set; } // Moved to extention method
     }
 }
